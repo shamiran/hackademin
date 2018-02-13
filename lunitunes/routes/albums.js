72af9ad0f20929a88de339c40ec5d4ce89621e6d
Shamiran Jaf <shamiran.jaf@gmail.com>
2018-02-13 16:54:57 +0100

    Add EJS

--- lunitunes/routes/albums.js
+++ lunitunes/routes/albums.js
@@ -8,18 +8,24 @@ const db = new sqlite3.Database('chinook.db', sqlite3.OPEN_READWRITE, (err) => {
 });
 
 router.get('/', function (req, res, next) {
-  db.serialize((albums) => {
-    db.all('SELECT *  FROM Album;', (err, rows) => {
+  db.serialize( () => {
+    let sqlStatement = 'SELECT Title, Artist.Name AS ArtistName, Artist.ArtistId AS ArtistId, Album.AlbumId AS AlbumId FROM Album LEFT OUTER JOIN Artist ON Album.ArtistId = Artist.ArtistId;'
+    db.all(sqlStatement, (err, rows) => {
       if (err) {
         console.error(err.message)
       } else {
         console.log("Rows: ", rows)
         let albums = []
         rows.forEach((row) => {
-          albums.push(row.Title)
+          albums.push({
+            title: row.Title,
+            artistName: row.ArtistName,
+            albumId: row.AlbumId,
+            artistId: row.ArtistId
+            })
         });
         res.render('albums', {
-          title: 'Express',
+          title: 'Lunitunes Albums',
           albums: albums
         });
         console.log("Albums: ", albums)
@@ -27,6 +33,35 @@ router.get('/', function (req, res, next) {
     })
   });
 
+});
+
+router.get('/:albumId', function (req, res, next) {
+  let albumId = req.params.albumId;
+  db.serialize( () => {
+    console.log("Hello")
+    let sql = ('SELECT Track.Name AS TrackName, Album.Title AS AlbumName, Track.UnitPrice AS UnitPrice, Artist.Name AS ArtistName  FROM Track LEFT OUTER JOIN Album ON Track.AlbumId = Album.AlbumId LEFT OUTER JOIN ARTIST ON Album.ArtistId = Artist.ArtistId WHERE Track.AlbumId = ?;')
+    db.all(sql, albumId, (err, rows) => {
+      if (err) {
+        return res.status(500);
+      } else {
+        console.log("Boom")
+        let albumTitle = rows[0].AlbumName
+        let tracks = []
+        rows.forEach( (row) => {
+          tracks.push({
+            name: row.TrackName,
+            albumName:  row.AlbumName,
+            artistName: row.ArtistName,
+            price: row.UnitPrice
+          })
+        })
+        res.render('album', {
+          title: albumTitle,
+          tracks: tracks
+        })
+      }
+    })
+  })
 });
 
 module.exports = router;
